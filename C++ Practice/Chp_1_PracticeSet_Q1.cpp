425bda612f87ed8b6fe67ab4f19489d4151d2aa6
Raihan Mhate <codinraiho@gmail.com>
2025-03-05 13:03:20 +0530

    Area of rectangle from 1st Chp

---
+++ C++ Practice/Chp_1_PracticeSet_Q1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int l=2, b=4;
+    int AreaRectangle=l*b;
+
+    cout<<"The area of rectangle is\n"<<AreaRectangle<<endl;
+
+    return 0;
+}
