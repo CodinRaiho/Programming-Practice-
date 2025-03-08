91fb4772898d3c2c1668c683f68bcc4e6d17c561
Raihan Mhate <codinraiho@gmail.com>
2025-03-08 12:25:44 +0530

    Area of circle with c++ from chp 1 done

---
+++ C++ Practice/Chp_1_PracticeSet_Q2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int Radius=3;
+    double AreaCircle=3.14*Radius*Radius;
+
+    cout<<"The Area of the circle is\n"<<AreaCircle<<endl;
+    
+    return 0;
+}
