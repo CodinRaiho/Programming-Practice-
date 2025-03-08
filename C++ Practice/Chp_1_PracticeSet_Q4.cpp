429bccd5723e4ba1465ab0fcbf00aa8e1322501f
Raihan Mhate <codinraiho@gmail.com>
2025-03-08 12:51:10 +0530

    SimpleInterest using C++ from 1st chp

---
+++ C++ Practice/Chp_1_PracticeSet_Q4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int principal=100, rate=4, years=1;
+    int simpleInterest=(principal*rate*years)/100;
+
+    cout<<"The simple interest is: "<<simpleInterest<<endl;
+
+    return 0;
+}
